1aaac6d0611f96c72bbde0dd2bef7c85134fa9d4
vitoriasilva <vitoria.c.faria@ufv.br>
2024-11-19 08:59:31 -0300

    coloquei condicional e a variavel flag

--- ccf110.c
+++ ccf110.c
@@ -2,32 +2,33 @@
 #include <string.h>
 int main()
 {
-char listaReceita [20][200];
-char opcao[200];
-
+    char listaReceita [20][200];
+    char opcao[200];
+    int encontrou = 0;
 
     strcpy(listaReceita[0],"Pizza");
-    strcpy(listaReceita[1],"bolo de chocolate");
-    strcpy(listaReceita[2],"barra de chocolate");
-    strcpy(listaReceita[3],"pastel");
-    strcpy(listaReceita[4],"temaki");
-    strcpy(listaReceita[5],"Feijoada Carioca");
-    strcpy(listaReceita[6],"Tacos de Carne Moída" );
-    strcpy(listaReceita[7],"Salmão Grelhado com Molho de Mostarda e Mel");
-    strcpy(listaReceita[8],"Hambúrguer Caseiro de Carne e Bacon");
-    strcpy(listaReceita[9],"Chili com Carne e Feijão");
-    strcpy(listaReceita[10],"coxinha de frango");
-    strcpy(listaReceita[11],"bife com molho");
-    strcpy(listaReceita[12],"torta de limão");
-    strcpy(listaReceita[13],"lasanha de carne com queijo");
-    strcpy(listaReceita[14],"Escondidinho de Carne Seca com Purê de Mandioquinha");
-    strcpy(listaReceita[15],"Creme Brulée");
-    strcpy(listaReceita[16],"Tabule de Quinoa");
-    strcpy(listaReceita[17],"Sopa de Abóbora com Gengibre");
-    strcpy(listaReceita[18],"Espaguete ao Pesto de Manjericão");
-    strcpy(listaReceita[19],"Moqueca Baiana de Peixe");
+    strcpy(listaReceita[1],"bolo de chocolate\n");
+    strcpy(listaReceita[2],"barra de chocolate\n");
+    strcpy(listaReceita[3],"pastel\n");
+    strcpy(listaReceita[4],"temaki\n");
+    strcpy(listaReceita[5],"Feijoada Carioca\n");
+    strcpy(listaReceita[6],"Tacos de Carne Moida\n" );
+    strcpy(listaReceita[7],"Salmao Grelhado com Molho de Mostarda e Mel\n");
+    strcpy(listaReceita[8],"Hamburguer Caseiro de Carne e Bacon\n");
+    strcpy(listaReceita[9],"Chili com Carne e Feijao\n");
+    strcpy(listaReceita[10],"coxinha de frango\n");
+    strcpy(listaReceita[11],"bife com molho\n");
+    strcpy(listaReceita[12],"torta de limao\n");
+    strcpy(listaReceita[13],"lasanha de carne com queijo\n");
+    strcpy(listaReceita[14],"Escondidinho de Carne Seca com Pure de Mandioquinha\n");
+    strcpy(listaReceita[15],"Creme Brulee\n");
+    strcpy(listaReceita[16],"Tabule de Quinoa\n");
+    strcpy(listaReceita[17],"Sopa de Abobora com Gengibre\n");
+    strcpy(listaReceita[18],"Espaguete ao Pesto de Manjericao\n");
+    strcpy(listaReceita[19],"Moqueca Baiana de Peixe\n");
 
-    scanf("%s",&opcao);
+    fgets(opcao, 200, stdin);
+
     int quantidade [20];
     quantidade[0] = 13;
     quantidade[1] = 11;
@@ -59,11 +60,20 @@ char opcao[200];
 
             printf("%d\n",quantidade[i]);
 
+            encontrou =1;
+
+        }
+        if(encontrou == 1)
+        {
             break;
         }
-
-
+    }
+    if(encontrou == 0)
+    {
+        printf("nao encontrou\n");
     }
 
 
+
+    return 0;
 }
